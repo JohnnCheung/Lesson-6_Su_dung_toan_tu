37a6f88c9a92d79f24debc859dcc336ff7c81eed
Dung <tr.manh.dung@gmail.com>
2023-06-10 09:32:57 +0700

    Nop bai tap

---
+++ code.js
@@ -0,0 +1,4 @@
+let inputWidth = parseInt(prompt("Enter the width: "));
+let inputHeigh = parseInt(prompt("Enter the heigh: "));
+let area = inputWidth * inputHeigh;
+document.write("The area is: " + area);
